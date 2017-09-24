dc2841f853e8050419920a9be452cc0869358b17
Ken Grønnbeck <kengroenn@gmail.com>
2017-09-24 12:48:07 +0200

    Start with volume (optional)

--- docker-neo4j/start.sh
+++ docker-neo4j/start.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+_volume=$1
+
+if [ -z "$_volume" ]; then
+  _use_volume=false
+fi
+
 docker-machine start dev
 eval $(docker-machine env dev)
 
-docker run \
--e "NEO4J_AUTH=neo4j/password" \
--p 7474:7474 \
--p 7687:7687 \
-neo4j
+if [ "$_use_volume" = false ]; then
+  docker run \
+  -e "NEO4J_AUTH=neo4j/password" \
+  -p 7474:7474 \
+  -p 7687:7687 \
+  neo4j
+else
+  docker run \
+  -v $_volume:/var/lib/neo4j/import \
+  -e "NEO4J_AUTH=neo4j/password" \
+  -p 7474:7474 \
+  -p 7687:7687 \
+  neo4j
+fi
